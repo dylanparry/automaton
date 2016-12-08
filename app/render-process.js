47f8cadf9e40be088b069da107d1d57be8c905ed
Dylan Parry <dylan.parry@bluefoxtech.co.uk>
2016-12-08 11:29:05 +0000

    Add basic render process with placeholder router

---
+++ app/render-process.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { webFrame } from 'electron';
+
+import Router from './components/routing/router';
+
+// Disable zooming
+webFrame.setZoomLevelLimits(1, 1);
+
+ReactDOM.render(React.createElement(Router), document.getElementById('app'));
