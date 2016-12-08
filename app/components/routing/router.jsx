1cd488fc5a869ec49f6cfc7ae39d0d9ad4e615e9
Dylan Parry <dylan.parry@bluefoxtech.co.uk>
2016-12-08 11:47:44 +0000

    Add routes

--- app/components/routing/router.jsx
+++ app/components/routing/router.jsx
@@ -1,3 +1,25 @@
 import React from 'react';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
-export default () => <p>Routes</p>;
+export default () => (
+    <Router history={hashHistory}>
+        <Route path="/" component={null}> {/* Main app template */}
+            <IndexRoute component={null} /> {/* Home screen */}
+
+            <Route path="heating">
+                <IndexRoute component={null} /> {/* Heating home screen */}
+                <Route path=":roomId" component={null} /> {/* Room summary */}
+            </Route>
+
+            <Route path="weather">
+                <IndexRoute component={null} /> {/* Summary of weather now */}
+                <Route path="weather/today" component={null} /> {/* Summary of today's weather */}
+                <Route path="weather/week" component={null} /> {/* Five day forecast */}
+            </Route>
+
+            <Route path="settings">
+                <IndexRoute component={null} /> {/* Settings home screen */}
+            </Route>
+        </Route>
+    </Router>
+);
