ab23addfca7bf699cf7c6469e9f338dc782bf025
Dylan Parry <dylan.parry@bluefoxtech.co.uk>
2016-12-08 17:36:02 +0000

    Implement Metro style menu

--- app/views/app.jsx
+++ app/views/app.jsx
@@ -4,14 +4,24 @@ import { Link } from 'react-router';
 const App = ({ children }) => (
     <div>
         <div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/heating">Heating Summary</Link></li>
-                <li><Link to="/heating/1">Heating Room Summary</Link></li>
-                <li><Link to="/weather">Weather Summary</Link></li>
-                <li><Link to="/weather/today">Weather Today</Link></li>
-                <li><Link to="/weather/week">Weather Week</Link></li>
-                <li><Link to="/settings">Settings</Link></li>
+            <ul className="sidebar2">
+                <li><Link to="/"><span className="icon fa fa-fw fa-home" /> Home</Link></li>
+                <li>
+                    <a className="dropdown-toggle"><span className="icon fa fa-fw fa-thermometer" /> Heating</a>
+                    <ul className="d-menu" data-role="dropdown">
+                        <li><Link to="/heating">Heating Summary</Link></li>
+                        <li><Link to="/heating/1">Heating Room Summary</Link></li>
+                    </ul>
+                </li>
+                <li>
+                    <a className="dropdown-toggle"><span className="icon fa fa-fw fa-sun-o" /> Weather</a>
+                    <ul className="d-menu" data-role="dropdown">
+                        <li><Link to="/weather">Weather Summary</Link></li>
+                        <li><Link to="/weather/today">Weather Today</Link></li>
+                        <li><Link to="/weather/week">Weather Week</Link></li>
+                    </ul>
+                </li>
+                <li><Link to="/settings"><span className="icon fa fa-fw fa-cog" /> Settings</Link></li>
             </ul>
         </div>
         <div>
