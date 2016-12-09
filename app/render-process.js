df06cf527f0394a2218054b16646858c716dbcc5
Dylan Parry <dylan.parry@bluefoxtech.co.uk>
2016-12-09 11:09:32 +0000

    Renme directory from router to navigation

--- app/render-process.js
+++ app/render-process.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { webFrame } from 'electron';
 
-import Router from './components/routing/router';
+import Router from './components/navigation/router';
 
 // Disable zooming
 webFrame.setZoomLevelLimits(1, 1);
