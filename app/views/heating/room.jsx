d3e9d35b167bf133097568c49e70bc450c9f249b
Dylan Parry <dylan.parry@bluefoxtech.co.uk>
2016-12-12 22:04:36 +0000

    Use room from store

--- app/views/heating/room.jsx
+++ app/views/heating/room.jsx
@@ -1,23 +1,45 @@
 import React from 'react';
+import { inject, observer, observableMap } from 'mobx-react';
 
 import BackButton from '../../components/buttons/back-button';
 import RoomTile from '../../components/heating/room-tile';
 import DeviceTile from '../../components/heating/device-tile';
 
-export default () => (
-    <div>
-        <div className="margin20">
-            <BackButton to="/heating">Heating Summary</BackButton>
+const Room = ({ heatingStore, params }) => {
+    // Get the room specified by the URL params
+    const room = heatingStore.rooms.values().find(r => r.id === parseInt(params.roomId, 10));
 
-            <h1>Example Room</h1>
+    // Return a null component if no room found
+    if (typeof room === 'undefined') {
+        return null;
+    }
+
+    return (
+        <div>
+            <div className="margin20">
+                <BackButton to="/heating">Heating Summary</BackButton>
+
+                <h1>{room.name}</h1>
+            </div>
+
+            <div className="tile-container">
+                <RoomTile id={room.id} label={room.name} displayWide />
+
+                <DeviceTile label="Device 1" />
+                <DeviceTile label="Device 2" />
+                <DeviceTile label="Device 3" />
+            </div>
         </div>
+    );
+};
 
-        <div className="tile-container">
-            <RoomTile id={1} label="Room 1" displayWide />
+Room.propTypes = {
+    heatingStore: React.PropTypes.shape({
+        rooms: observableMap,
+    }),
+    params: React.PropTypes.shape({
+        roomId: React.PropTypes.string,
+    }),
+};
 
-            <DeviceTile label="Device 1" />
-            <DeviceTile label="Device 2" />
-            <DeviceTile label="Device 3" />
-        </div>
-    </div>
-);
+export default inject('heatingStore')(observer(Room));
