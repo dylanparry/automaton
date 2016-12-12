0223483ced666b4b4a7c604032c93bafae582660
Dylan Parry <dylan.parry@bluefoxtech.co.uk>
2016-12-12 15:01:16 +0000

    Provide a heating store

--- app/render-process.js
+++ app/render-process.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react';
 import { webFrame } from 'electron';
 
 import Router from './components/navigation/router';
+import HeatingStore from './stores/heating-store';
 
 // Disable zooming
 webFrame.setZoomLevelLimits(1, 1);
 
-ReactDOM.render(React.createElement(Router), document.getElementById('app'));
+// Stores
+const heatingStore = new HeatingStore();
+
+// Create a Provider with props for the stores and the Router as a child
+const provider = React.createElement(Provider, {
+    heatingStore,
+}, React.createElement(Router));
+
+ReactDOM.render(provider, document.getElementById('app'));
