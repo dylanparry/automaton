ba98e163a8cad99268fa8f6fd5e42986fa1b6a6e
Dylan Parry <dylan.parry@bluefoxtech.co.uk>
2016-12-12 18:07:19 +0000

    Use rooms from heating store

--- app/components/navigation/app-bar.jsx
+++ app/components/navigation/app-bar.jsx
@@ -1,34 +1,43 @@
 import React from 'react';
+import { inject, observer, observableMap } from 'mobx-react';
 import { Link } from 'react-router';
 
-export default () => (
-    <div className="app-bar navy" style={{ position: 'fixed', top: 0, left: 0, zIndex: 2 }}>
-        <Link to="/" className="app-bar-element"><span className="fa fa-fw fa-cogs" /> automaton</Link>
-        <span className="app-bar-divider" />
-        <ul className="app-bar-menu">
-            <li>
-                <a className="dropdown-toggle"><span className="fa fa-fw fa-thermometer" /> Central Heating</a>
-                <ul className="d-menu navy" data-role="dropdown">
-                    <li><Link to="/heating">Heating Summary</Link></li>
-                    <li className="menu-title">Heating by Room</li>
-                    <li><Link to="/heating/1">Room 1</Link></li>
-                    <li><Link to="/heating/2">Room 2</Link></li>
-                    <li><Link to="/heating/3">Room 3</Link></li>
-                    <li><Link to="/heating/4">Room 4</Link></li>
-                    <li><Link to="/heating/5">Room 5</Link></li>
-                    <li><Link to="/heating/6">Room 6</Link></li>
-                    <li><Link to="/heating/7">Room 7</Link></li>
-                </ul>
-            </li>
-            <li>
-                <a className="dropdown-toggle"><span className="fa fa-fw fa-cloud" /> Weather</a>
-                <ul className="d-menu" data-role="dropdown">
-                    <li><Link to="/weather">Weather Summary</Link></li>
-                    <li><Link to="/weather/today">Weather Today</Link></li>
-                    <li><Link to="/weather/week">Weather Week</Link></li>
-                </ul>
-            </li>
-            <li><Link to="/settings"><span className="fa fa-fw fa-cog" /> Settings</Link></li>
-        </ul>
-    </div>
-);
+const AppBar = ({ heatingStore }) => {
+    const rooms = heatingStore.rooms.values().map(room => (
+        <li key={room.id}><Link to={`/heating/${room.id}`}>{room.name}</Link></li>
+    ));
+
+    return (
+        <div className="app-bar navy" style={{ position: 'fixed', top: 0, left: 0, zIndex: 2 }}>
+            <Link to="/" className="app-bar-element"><span className="fa fa-fw fa-cogs" /> automaton</Link>
+            <span className="app-bar-divider" />
+            <ul className="app-bar-menu">
+                <li>
+                    <a className="dropdown-toggle"><span className="fa fa-fw fa-thermometer" /> Central Heating</a>
+                    <ul className="d-menu navy" data-role="dropdown">
+                        <li><Link to="/heating">Heating Summary</Link></li>
+                        <li className="menu-title">Heating by Room</li>
+                        {rooms}
+                    </ul>
+                </li>
+                <li>
+                    <a className="dropdown-toggle"><span className="fa fa-fw fa-cloud" /> Weather</a>
+                    <ul className="d-menu" data-role="dropdown">
+                        <li><Link to="/weather">Weather Summary</Link></li>
+                        <li><Link to="/weather/today">Weather Today</Link></li>
+                        <li><Link to="/weather/week">Weather Week</Link></li>
+                    </ul>
+                </li>
+                <li><Link to="/settings"><span className="fa fa-fw fa-cog" /> Settings</Link></li>
+            </ul>
+        </div>
+    );
+};
+
+AppBar.propTypes = {
+    heatingStore: React.PropTypes.shape({
+        rooms: observableMap,
+    }),
+};
+
+export default inject('heatingStore')(observer(AppBar));
--- app/views/heating/index.jsx
+++ app/views/heating/index.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
+import { inject, observer, observableMap } from 'mobx-react';
 
 import BackButton from '../../components/buttons/back-button';
 import HeatingTile from '../../components/heating';
 import RoomTile from '../../components/heating/room-tile';
 
-export default () => (
-    <div>
-        <div className="margin20">
-            <BackButton to="/">Home</BackButton>
+const HeatingIndex = ({ heatingStore }) => {
+    const rooms = heatingStore.rooms.values().map(room => (
+        <RoomTile key={room.id} id={room.id} label={room.name} />
+    ));
 
-            <h1>Heating Summary</h1>
+    return (
+        <div>
+            <div className="margin20">
+                <BackButton to="/">Home</BackButton>
+
+                <h1>Heating Summary</h1>
+            </div>
+
+            <div className="tile-container">
+                <HeatingTile />
+
+                {rooms}
+            </div>
         </div>
+    );
+};
 
-        <div className="tile-container">
-            <HeatingTile />
+HeatingIndex.propTypes = {
+    heatingStore: React.PropTypes.shape({
+        rooms: observableMap,
+    }),
+};
 
-            <RoomTile id={1} label="Room 1" />
-            <RoomTile id={2} label="Room 2" />
-            <RoomTile id={3} label="Room 3" />
-            <RoomTile id={4} label="Room 4" />
-            <RoomTile id={5} label="Room 5" />
-            <RoomTile id={6} label="Room 6" />
-            <RoomTile id={7} label="Room 7" />
-        </div>
-    </div>
-);
+export default inject('heatingStore')(observer(HeatingIndex));
