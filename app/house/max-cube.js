4ac6dbfb887f3436f295c2ccb0615f5c6f2181ae
Dylan Parry <dylan.parry@bluefoxtech.co.uk>
2016-12-12 15:00:50 +0000

    Add basic cube implementation

---
+++ app/house/max-cube.js
@@ -0,0 +1,44 @@
+import net from 'net';
+import { observable } from 'mobx';
+
+export default class MaxCube {
+    @observable isConnected = false;
+    socket = new net.Socket();
+    ipAddress = null;
+    port = null;
+
+    constructor({ ipAddress, port }) {
+        this.ipAddress = ipAddress;
+        this.port = port;
+    }
+
+    // Connects to the Cube and returns a promise
+    connect() {
+        // If already connected, just resolve the promise
+        if (this.isConnected) {
+            return Promise.resolve();
+        }
+
+        // Return a new promise that resolves when the connection is established
+        return new Promise((resolve) => {
+            this.socket.connect({ port: this.port, host: this.ipAddress }, () => {
+                this.isConnected = true;
+                resolve();
+            });
+        });
+    }
+
+    // Closes the connection to the cube
+    disconnect() {
+        this.isConnected = false;
+        this.socket.destroy();
+    }
+
+    // Calls the callback function with string data from the buffer
+    listen(callback) {
+        this.socket.on('data', (buffer) => {
+            const data = buffer.toString('utf-8');
+            callback(data);
+        });
+    }
+}
