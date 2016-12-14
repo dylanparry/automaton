d76cff1a095151806a3d4c8ef0de1a48ef001b83
Dylan Parry <dylan.parry@bluefoxtech.co.uk>
2016-12-14 14:17:22 +0000

    Add observable properties to devices

--- app/devices/wall-mounted-thermostat.js
+++ app/devices/wall-mounted-thermostat.js
@@ -1,5 +1,18 @@
+import { observable } from 'mobx';
+
 import Device from './device';
 
 export default class WallMountedThermostat extends Device {
-
+    @observable valid = null;
+    @observable error = null;
+    @observable answer = null;
+    @observable initialised = null;
+    @observable battery = null;
+    @observable linkStatus = null;
+    @observable panel = null;
+    @observable gateway = null;
+    @observable dst = null;
+    @observable mode = null;
+    @observable setPoint = 0;
+    @observable actualTemperature = 0;
 }
