9d82fb0e89bc47f578d3960496a5dc48be8b9779
Dylan Parry <dylan.parry@bluefoxtech.co.uk>
2016-12-17 17:07:48 +0000

    Convert to stateless component

--- app/components/chart/temperature-chart.jsx
+++ app/components/chart/temperature-chart.jsx
@@ -1,47 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Chartist from 'chartist';
 import ChartistGraph from 'react-chartist';
 import moment from 'moment';
 
-class TemperatureChart extends Component {
-    render() {
-        if (this.props.data.length === 0) {
-            return null;
-        }
-
-        const data = {
-            series: [
-                {
-                    name: 'temperature',
-                    data: this.props.data.map(item => ({ x: item.created, y: item.temperature })),
-                },
-            ],
-        };
-
-        const options = {
-            axisX: {
-                type: Chartist.FixedScaleAxis,
-                divisor: 12,
-                labelInterpolationFnc: value => moment(value).format('HH'),
-            },
-            axisY: {
-                low: 15,
-                onlyInteger: true,
-            },
-            series: {
-                temperature: {
-                    showPoint: false,
-                    showArea: true,
-                },
-            },
-
-        };
-
-        return (
-            <ChartistGraph data={data} options={options} type="Line" />
-        );
+const TemperatureChart = ({ data }) => {
+    if (data.length === 0) {
+        return null;
     }
-}
+
+    const processedData = {
+        series: [
+            {
+                name: 'temperature',
+                data: data.map(item => ({ x: item.created, y: item.temperature })),
+            },
+        ],
+    };
+
+    const options = {
+        axisX: {
+            type: Chartist.FixedScaleAxis,
+            divisor: 12,
+            labelInterpolationFnc: value => moment(value).format('HH'),
+        },
+        axisY: {
+            low: 15,
+            onlyInteger: true,
+        },
+        series: {
+            temperature: {
+                showPoint: false,
+                showArea: true,
+            },
+        },
+
+    };
+
+    return (
+        <ChartistGraph data={processedData} options={options} type="Line" />
+    );
+};
 
 TemperatureChart.propTypes = {
     data: React.PropTypes.arrayOf(React.PropTypes.shape({
