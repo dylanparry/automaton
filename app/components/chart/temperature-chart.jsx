630a4d5b0d0261f6974bc6b6eed1bf1b2130d7db
Dylan Parry <dylan.parry@bluefoxtech.co.uk>
2016-12-18 12:43:24 +0000

    Round to nearest full degree instead

--- app/components/chart/temperature-chart.jsx
+++ app/components/chart/temperature-chart.jsx
@@ -5,9 +5,6 @@ import moment from 'moment';
 
 import AxisTitle from '../../utils/chartist-plugin-axistitle';
 
-// Rounds a number to the nearest 0.5 value
-const roundToHalf = number => Math.round(number * 2) / 2;
-
 const TemperatureChart = ({ data, currentTemperature }) => {
     if (data.length === 0) {
         return null;
@@ -18,8 +15,8 @@ const TemperatureChart = ({ data, currentTemperature }) => {
             {
                 name: 'temperature',
                 data: Object.assign(
-                    data.map(item => ({ x: item.created, y: roundToHalf(item.temperature) })),
-                    { x: new Date(), y: roundToHalf(currentTemperature) },
+                    data.map(item => ({ x: item.created, y: Math.round(item.temperature) })),
+                    { x: new Date(), y: Math.round(currentTemperature) },
                 ),
             },
         ],
