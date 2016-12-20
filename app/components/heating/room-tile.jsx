7c9433429d571fbb728c563792d7908df6532aea
Dylan Parry <dylan.parry@bluefoxtech.co.uk>
2016-12-20 10:58:46 +0000

    Refactor

--- app/components/heating/room-tile.jsx
+++ app/components/heating/room-tile.jsx
@@ -6,85 +6,87 @@ import DeviceConstants from '../../constants/device';
 import ClassBuilder from '../../utils/class-builder';
 import Room from '../../house/room';
 
+const getBackgroundColour = (room) => {
+  // Check for errors
+  if (room.hasErrors) {
+    return 'bg-red';
+  }
+
+  // When the radiators are on, and the room is cold
+  if (room.radiatorsOn && room.isCold) {
+    return 'bg-lighterBlue';
+  }
+
+  // If there are active programs, and the room is warm
+  if (room.setPoint > 10) {
+    return 'bg-amber';
+  }
+
+  // Otherwise, just colour it grey
+  return 'bg-grayLight';
+};
+
+const getMode = (room) => {
+  // Check for errors
+  if (room.hasErrors) {
+    return 'error';
+  }
+
+  // When the room program is over, and it's not in boost or holiday mode, it's 'off'
+  if (
+    room.setPoint <= 10 &&
+    room.mode !== DeviceConstants.Status.BOOST &&
+    room.mode !== DeviceConstants.Status.HOLIDAY) {
+    return 'off';
+  }
+
+  const modes = [
+    [DeviceConstants.Status.AUTO, 'auto'],
+    [DeviceConstants.Status.MANUAL, 'manual'],
+    [DeviceConstants.Status.HOLIDAY, 'holiday'],
+    [DeviceConstants.Status.BOOST, 'boost'],
+  ];
+
+  return modes[room.mode][1];
+};
+
+const getIcon = (room) => {
+  const tests = [
+    ['office', 'flaticon-office'],
+    ['stairs', 'flaticon-stairs'],
+    ['bedroom', 'flaticon-bedroom'],
+    ['living', 'flaticon-livingroom'],
+    ['kitchen', 'flaticon-kitchen'],
+    ['sewing', 'flaticon-sewing'],
+  ];
+
+  for (let i = 0; i < tests.length; i += 1) {
+    const test = tests[i];
+    const regexp = new RegExp(test[0], 'i');
+
+    if (room.name.match(regexp)) {
+      return test[1];
+    }
+  }
+
+  return null;
+};
+
 const RoomTile = ({ room, displayWide }) => {
   const iconClass = new ClassBuilder();
   iconClass.useIcon();
-  if (room.name.match(/office/i)) {
-    iconClass.iconName = 'flaticon-office';
-  }
-  else if (room.name.match(/stairs/i)) {
-    iconClass.iconName = 'flaticon-stairs';
-  }
-  else if (room.name.match(/bed/i)) {
-    iconClass.iconName = 'flaticon-bedroom';
-  }
-  else if (room.name.match(/living/i)) {
-    iconClass.iconName = 'flaticon-livingroom';
-  }
-  else if (room.name.match(/kitchen/i)) {
-    iconClass.iconName = 'flaticon-kitchen';
-  }
-  else if (room.name.match(/sewing/i)) {
-    iconClass.iconName = 'flaticon-sewing';
-  }
+  iconClass.iconName = getIcon(room);
 
   const tileClass = new ClassBuilder();
   tileClass.tile = displayWide ? 'tile-wide' : 'tile';
   tileClass.color = 'fg-white';
   tileClass.useTextShadow();
-
-  // Decide what colour the tile should be
-  if (room.radiatorsOn && room.isCold) {
-    tileClass.background = 'bg-lighterBlue';
-  }
-  else if (room.setPoint > 10) {
-    tileClass.background = 'bg-amber';
-  }
-  else {
-    tileClass.background = 'bg-grayLight';
-  }
-
-  let mode;
-  switch (room.mode) {
-    case DeviceConstants.Status.AUTO:
-      mode = 'auto';
-      break;
-    case DeviceConstants.Status.MANUAL:
-      mode = 'manual';
-      break;
-    case DeviceConstants.Status.HOLIDAY:
-      mode = 'holiday';
-      break;
-    case DeviceConstants.Status.BOOST:
-      mode = 'boost';
-      break;
-    default:
-      break;
-  }
-
-  if (
-    room.setPoint <= 10 &&
-    room.mode !== DeviceConstants.Status.BOOST &&
-    room.mode !== DeviceConstants.Status.HOLIDAY) {
-    mode = 'off';
-  }
+  tileClass.background = getBackgroundColour(room);
 
   // If any device in the room is reporting errors, change the colour and show an icon
-  let statusIcon;
-  for (let i = 0; i < room.devices.length; i += 1) {
-    const device = room.devices[i];
-    if (
-      device.valid === DeviceConstants.Valid.INVALID ||
-      device.error === DeviceConstants.Error.YES ||
-      device.initialised === DeviceConstants.Initialised.NO ||
-      device.battery === DeviceConstants.Battery.LOW ||
-      device.linkStatus === DeviceConstants.LinkStatus.ERROR
-    ) {
-      tileClass.background = 'bg-red';
-      mode = 'error';
-      statusIcon = <span className="tile-badge top right"><span className="fa fa-fw fa-warning" /></span>;
-    }
-  }
+  const statusIcon = room.hasErrors
+    ? <span className="tile-badge top right"><span className="fa fa-fw fa-warning" /></span>
+    : null;
 
   return (
     <Link to={`/heating/${room.id}`}>
@@ -93,7 +95,7 @@ const RoomTile = ({ room, displayWide }) => {
           <span className={iconClass} />
           <span className="tile-label">{room.name}</span>
           <span className="tile-badge">{room.actualTemperature ? `${room.actualTemperature}°C` : null}</span>
-          <span className="tile-badge top left">{mode}</span>
+          <span className="tile-badge top left">{getMode(room)}</span>
           {statusIcon}
         </div>
       </div>
