f8e38c502c4817155b2c1cf65e46fa68596aebb3
Dylan Parry <dylan.parry@bluefoxtech.co.uk>
2017-03-20 14:57:32 +0000

    Add butons and titles - Add weather tile to summary - Add buttons to return to summary from forecasts

--- app/views/weather/10day.jsx
+++ app/views/weather/10day.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import BackButton from '../../components/buttons/back-button';
+
 export default () => (
-  <p>Weather 10 Day Forecast</p>
+  <div>
+    <div className="margin20">
+      <BackButton to="/weather">Weather Summary</BackButton>
+
+      <h1>10 Day Forecast</h1>
+    </div>
+  </div>
 );
--- app/views/weather/index.jsx
+++ app/views/weather/index.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
+import WeatherTile from '../../components/weather';
+import BackButton from '../../components/buttons/back-button';
+
 export default () => (
-  <p>Weather Summary</p>
+  <div>
+    <div className="margin20">
+      <BackButton to="/">Home</BackButton>
+
+      <h1>Weather Summary</h1>
+    </div>
+
+    <div className="tile-container">
+      <WeatherTile />
+    </div>
+  </div>
 );
