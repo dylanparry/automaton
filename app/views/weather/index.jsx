f2ec3cccde3322ba893645c5f013be5119e5477b
Dylan Parry <dylan.parry@bluefoxtech.co.uk>
2017-03-21 15:11:24 +0000

    Add tiles for summary (empty) and forecast buttons

--- app/views/weather/index.jsx
+++ app/views/weather/index.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import WeatherTile from '../../components/weather';
 import BackButton from '../../components/buttons/back-button';
+
+const style = {
+  container: {
+    marginTop: 45,
+    textAlign: 'center',
+  },
+
+  buttonText: {
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+};
 
 export default () => (
   <div>
@@ -13,6 +26,28 @@ export default () => (
 
     <div className="tile-container">
       <WeatherTile />
+
+      <div className="tile-large bg-white" />
+
+      <Link to="/weather/24hour">
+        <div className="tile bg-teal fg-white text-shadow">
+          <div className="tile-content">
+            <div style={style.container}>
+              <span style={style.buttonText}>24hr<br />Forecast</span>
+            </div>
+          </div>
+        </div>
+      </Link>
+
+      <Link to="/weather/10day">
+        <div className="tile bg-teal fg-white text-shadow">
+          <div className="tile-content">
+            <div style={style.container}>
+              <span style={style.buttonText}>10 day<br />Forecast</span>
+            </div>
+          </div>
+        </div>
+      </Link>
     </div>
   </div>
 );
