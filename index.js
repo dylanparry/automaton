024b01e379f7e17508f6873bed5679fd5906dde3
Dylan Parry <dylan.parry@bluefoxtech.co.uk>
2022-11-28 13:45:25 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require("electron");
+
+/**
+ * Creates the main window
+ */
+function createMainWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadFile("hello-world.html");
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createMainWindow);
+
+// When all windows are closed exit the app, except in macOS.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+// When the application gets activated create the main window if one does not exist
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
